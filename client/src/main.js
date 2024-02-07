af90795a7be3078211ca8f5e0053fc2cfb894157
ahmedalthary <althryaa2@gmail.com>
2024-02-07 21:21:07 +0300

    Install Vue

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,17 @@
-import "./assets/tailwind.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import "./assets/tailwind.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 
 import App from "./App.vue";
 import router from "./router";
-
+// library.add(far);
 const app = createApp(App);
 
 app.use(createPinia());
+app.component("FontAwesome", FontAwesomeIcon);
 app.use(router);
 
 app.mount("#app");
