c79e9f18f47fdcf266ab9b096e7f63e5a94fe87c
ahmedalthary <althryaa2@gmail.com>
2024-02-11 21:46:41 +0300

    Fix some style in sign up page

--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,13 +1,13 @@
 <template>
     <section class="my-20">
-        <div class="flex gap-32 flex-row ">
-            <div class="w-[600px] rounded-b-3 overflow-hidden">
+        <div class="flex gap-24 flex-row ">
+            <div class=" rounded-b-3 flex-grow-[1] overflow-hidden">
                 <img src="../assets//img/signup.svg" class="h-full object-cover" alt="Signup">
             </div>
-            <div class="me-[135px] flex-grow-[1]">
-                <h1 class="h-medium-36 text-text-color2 mb-4">Create an account</h1>
+            <div class="me-[135px] w-[400px] self-center">
+                <h1 class="h-medium-36 text-text-color2 mb-3">Create an account</h1>
                 <p class="t-medium-16 text-text-color2">Enter your details below</p>
-                <form class="my-8 flex flex-col gap-10 ">
+                <form class="my-6 flex flex-col gap-10 ">
                     <input type="text"
                         class=" py-[7px]  placeholder:text-[16px] placeholder:font-normal text-[16px] text-text-color1 font-medium focus:outline-0  border-b focus:border-secondary2 "
                         placeholder="Name">
@@ -29,7 +29,7 @@
                     class="bg-transparent t-medium-16 text-text-color2 rounded-b-3 p-3 w-full border border-text-color1">Sign
                     up with
                     Google</button>
-                <p class="t-medium-16 text-text-color1  mt-8 ">Already have account? <span
+                <p class="t-medium-16 text-text-color1  mt-6 ">Already have account? <span
                         class="ms-4 pb-[2px] t-medium-16 text-text-color2 border-b-2 border-text-color1 cursor-pointer hover:text-secondary2">Log
                         in</span>
                 </p>
